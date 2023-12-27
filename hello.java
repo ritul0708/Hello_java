aede2d11df4054abba76f65870a5bbe44bdc8117
Ritul Jain <rituljain0708@gmail.com>
2023-12-27 14:01:59 +0530

    changed class name

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
-public class Hello {
-  public static void main(String[] args) {
-    System.out.println("Hello, Jenkins - From Java");
-  }
+public class hello {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, Jenkins - From Java");
+   }
 }
